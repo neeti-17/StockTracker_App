38faadcbf9da8a648bd74f2c4d3be02c608df883
Neeti Singh <neetisingh82541@gmail.com>
2024-10-08 23:43:29 +0530

    basic frontpage

--- stockproject/stock/views.py
+++ stockproject/stock/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def stockPicker(request):
+    return render(request,'stock/stockpicker.html')
+
+def stockTracker(request):
     return render(request,'stock/stocktracker.html')
